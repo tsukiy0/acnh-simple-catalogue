46ca14183c43676f45e2c50dc2a394b29f0be2a0
tsukiy0 <tsukiy0@users.noreply.github.com>
2020-08-14 12:10:12 +1000

    feat: add extension to zip header with value

---
+++ IngestUtility/CommunitySheetIngestService/ValueRangeExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Core.Shared;
+using Google.Apis.Sheets.v4.Data;
+
+namespace IngestUtility.CommunitySheetIngestService
+{
+    public static class ValueRangeExtensions
+    {
+        public class HeadersNotFoundException : BaseException { }
+        public static List<Dictionary<string, Object>> ZipValues(this ValueRange valueRange)
+        {
+            var rows = valueRange.Values;
+
+            IList<string> headers = rows.First().Select(_ => _.ToString()).ToList();
+
+            if (headers == null)
+            {
+                throw new HeadersNotFoundException();
+            }
+
+            return rows.Skip(1)
+                .Select(values => headers
+                    .Zip(values)
+                    .ToDictionary(_ => _.First, _ => _.Second)
+                )
+                .ToList();
+        }
+    }
+}
